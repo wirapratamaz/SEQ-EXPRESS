d16cbedcaefaed410ddc4d9b84a729cbe08d0087
wirapratamaz <wira@gringgo.co>
2023-05-07 22:06:10 +0800

    implementasi fungsi agregat

--- repository/userRepository.js
+++ repository/userRepository.js
@@ -1,4 +1,4 @@
-const { User } = require('../models/user');
+const { User, sequelize } = require('../models/user');
 
 module.exports = {
   async getAllUsers() {
@@ -8,4 +8,22 @@ module.exports = {
   async getUserById(id) {
     return User.findByPk(id);
   },
+
+  async getUserStats(){
+    try {
+      const result =  await User.findOne({ //mengambil hasil perhitungan dari tabel user
+        attributes :[
+          //*sequelize.fn -> memanggil fungsi agregat SQL
+          //*sequelize.col -> memilih kolom yang dihitung
+          [sequelize.fn('count', sequelize.col('*')), 'totalUsers'],
+          [sequelize.fn('average', sequelize.col('age')), 'averageAge'],
+        ],
+        raw: true
+      });
+      return result
+    } catch (error) {
+      console.log(error);
+      throw new Error('Something went wrong')
+    }
+  }
 };
