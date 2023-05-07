b25357d8e7b123bd4e345caf8254e5fd59a54788
wirapratamaz <wira@gringgo.co>
2023-05-07 22:33:10 +0800

    implementasi complex Aggregate function

--- repository/userRepository.js
+++ repository/userRepository.js
@@ -1,4 +1,4 @@
-const { User, sequelize } = require('../models/user');
+const { User, Order, sequelize } = require('../models/user');
 
 module.exports = {
   async getAllUsers() {
@@ -12,7 +12,7 @@ module.exports = {
   async getUserStats(){
     try {
       const result =  await User.findOne({ //mengambil hasil perhitungan dari tabel user
-        attributes :[
+        attributes :[ //select from database
           //*sequelize.fn -> memanggil fungsi agregat SQL
           //*sequelize.col -> memilih kolom yang dihitung
           [sequelize.fn('count', sequelize.col('*')), 'totalUsers'],
@@ -21,6 +21,34 @@ module.exports = {
         raw: true
       });
       return result
+    } catch (error) {
+      console.log(error);
+      throw new Error('Something went wrong')
+    }
+  },
+
+  //*complex Aggregate function
+  async getUserStatsByOrder(){
+    try {
+      const result = await User.findAll({
+        attributes: [
+          'id',
+          'name',
+          [sequelize.fn('count', sequelize.col('Orders.id')), 'totalOrders'],
+          [sequelize.fn('sum', sequelize.col('Orders.amount')), 'totalAmount'],
+        ],
+        include: [ //membuat join dengan User Model
+          {
+            model: Order,
+            attributes: [] // daftar atribut yang ingin dikecualikan
+          }
+        ],
+        // mengelompokkan hasil query berdasarkan id pengguna.
+        group: ['User.id'],
+        //mengembalikan hasil query tanpa metadata.
+        raw: true
+      });
+      return result;
     } catch (error) {
       console.log(error);
       throw new Error('Something went wrong')
