69efdb9d0fca09b2c31eff63d33a5ba9dd5645e3
wirapratamaz <wira@gringgo.co>
2023-05-10 11:21:40 +0800

    implementation morgan, add access.log

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const sequelize = require('./sequelize');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 // Import Routes
 const usersRoutes = require('./routes/userRoute');
@@ -16,16 +19,23 @@ sequelize
   .then(() => console.log('Database connected'))
   .catch((err) => console.error('Unable to connect to the database:', err));
 
+//membuat write stream ke file log
+// { flags: 'a' } menunjukkan bahwa stream harus ditambahkan ke akhir file
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a' })
+
 // Set up middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(multer().any()); // any() allows handling all types of HTTP requests
+app.use(morgan('combined', {
+  stream: accessLogStream
+}));
 
 // Use Routes
 app.use('/users', usersRoutes);
 app.use('/tasks', tasksRoutes);
-app.use('/emails', emailRoutes); //email route
+app.use('/emails', emailRoutes);
 
 // Start Server
 app.listen(3000, () => console.log('Server started on port 3000'));
